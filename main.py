e471976c536456336a1bfc44314bbc0c861de055
circlsun <circlsun@gmail.com>
2023-01-19 16:51:54 +0300

    Add new logic blink

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ TIC_TIMEOUT = 0.1
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        for _ in range(40):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(10):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
         await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(20):
+            await asyncio.sleep(0)
 
 
 def draw(canvas):
